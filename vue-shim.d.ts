3e5baecf868b3c5de3946f804e32ea40063b6709
Caitlin Hawley <caitlin.hawley@quore.com>
2022-03-18 15:49:04 -0500

    Added global files needed for typescript

---
+++ vue-shim.d.ts
@@ -0,0 +1,17 @@
+// Allow importing .vue files
+declare module '*.vue' {
+    import Vue, { VNode } from 'vue';
+    export default Vue;
+}
+declare module '*.svg' {
+    // DON'T DECLARE THIS INSIDE GLOBAL MODULE
+    type Svg = VNode;
+
+    const content: Svg;
+    export default content;
+}
+
+declare module '*.scss' {
+    const content: { [className: string]: string };
+    export = content;
+}
